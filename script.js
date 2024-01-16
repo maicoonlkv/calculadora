36837f927bcf22a21e3c2bf781a05fe5e6c7b3fe
maicoonlkv <maicon.kvictor@gmail.com>
2024-01-15 23:41:12 -0300

    CRIACAO SCRIPT E AJUSTES NO CÓD.

---
+++ script.js
@@ -0,0 +1,26 @@
+
+const numberOne = document.getElementById("numOne")
+const numberTwo = document.getElementById("numTwo").value
+console.log(numberOne)
+console.log(numberTwo)
+
+
+
+
+
+
+
+
+
+
+
+
+numberOne.addEventListener("click", cliquei)
+
+
+
+function cliquei() {
+    console.log("Cliquei no numero 1") 
+
+}
+
